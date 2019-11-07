ebd9fe334940e2a462593bd7488fcfa2f5238e49
Oliver Beneš <oliver.benes@pslib.cz>
2019-11-07 20:19:45 +0100

    Přidejte soubory projektu.

---
+++ Grading/GradeAvgList.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grading
+{
+    class GradeAvgList : ICustomList
+    {
+        private GradeAvg[] _gt;
+        private readonly int maxcnt;
+
+        public GradeAvgList(int maxcnt)
+        {
+            _gt = new GradeAvg[maxcnt];
+            this.maxcnt = maxcnt;
+        }
+
+        public int Count { 
+            get {
+                for (int i = 0; i < _gt.Length; i++)
+                {
+                    if (_gt[i] is null) return i;
+                } 
+                return _gt.Length;
+            } 
+        }
+
+        public int Length { 
+            get {
+                return _gt.Length;
+            }
+        }
+
+        /// <summary>
+        ///   Metoda přidá novou známku do souhrnu známek
+        /// </summary>
+        /// <param name="g">známka</param>
+        public GradeAvg Add(Grade g)
+        {
+            // 1)  zjistíme, zda předmět je již v _gt (a získáme index)
+            // 2)  pokud ne, vytvoříme nový pomocí Add(GradeAvg) (a získáme index)
+            // 3)  přes Get() získáme referenci na souhrnnou známku a aktualizujeme ji pomocí "g"
+
+            return Get(Add(new GradeAvg(g.Subject) { Count = 1, Score = g.Score }));
+        }
+
+        /// <summary>
+        ///   Vloží nové souhrnné hodnocení, nebo doplní (přepíše?) stávající
+        /// </summary>
+        /// <param name="g">souhrnná známka</param>
+        /// <returns>index nově vložené známky</returns>
+        public int Add(GradeAvg g)
+        {
+            //přidáváme nový GradeAvg a případně předtím zvětšíme pole: ResizeArray(ref _gt, maxcnt)
+            int idx = IndexOf(g);
+            if (idx == -1)
+            {
+                idx = Count;
+                if (idx == Length) ResizeArray(ref _gt, maxcnt);
+                _gt[idx] = g;
+            } else
+            {
+                _gt[idx].Count += g.Count;
+                _gt[idx].Score += g.Score;
+            }
+
+            return idx;
+        }
+
+        public bool Delete(GradeAvg g)
+        {
+            GradeAvg[] newAvg = new GradeAvg[_gt.Length-1]; 
+            for (int i = 0; i < IndexOf(g); i++)
+            {
+                newAvg[i] = _gt[i];
+            }
+            for(int i = IndexOf(g) + 1; i < _gt.Length; i++)
+            {
+                newAvg[i] = _gt[i];
+            }
+            _gt = newAvg;
+            return true;
+        }
+
+        public bool Delete(int position)
+        {
+            GradeAvg[] newAvg = new GradeAvg[_gt.Length - 1];
+            for (int i = 0; i < position; i++)
+            {
+                newAvg[i] = _gt[i];
+            } //nahrání prvků až do position
+            for (int i = position + 1;i<_gt.Length ; i++)
+            {
+                newAvg[i] = _gt[i];
+            } //přeskočí prvek na position a nahraje zbylé
+            _gt = newAvg;
+            return true;
+        }
+
+        public GradeAvg Get(int position)
+        {
+            return _gt[position];
+        }
+
+        public GradeAvg[] GetAll()
+        {
+            var result = new GradeAvg[Count];
+            for (int i = 0; i < Count; i++)
+            {
+                result[i] = _gt[i];
+            }
+            return result;
+        }
+
+        public int IndexOf(GradeAvg g)
+        {
+            return IndexOf(g.Subject);
+        }
+
+        public int IndexOf(string subject)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (subject == _gt[i].Subject)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private static void ResizeArray(ref GradeAvg[] oldArray, int growUp)
+        {
+            if (growUp < 0) throw new ArgumentOutOfRangeException("growUp", "Parametr musí být kladné číslo.");
+
+            GradeAvg[] newGradeAvgs = new GradeAvg[oldArray.Length + growUp];
+            for (int i = 0; i < oldArray.Length; i++)
+            {
+                newGradeAvgs[i] = oldArray[i];
+            }
+            oldArray = newGradeAvgs;
+        }
+    }
+}
---
+++ Grading/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using DisplayHelper;
+
+namespace Grading
+{
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CertificateTable table = new CertificateTable();
+
+            Display displayGrading = new Display(new Proportion() { Width = 40, TopLeft = new System.Drawing.Point(20, 3) });
+            displayGrading.AddItem(new LabelItem("-- Vysvědčení --"));
+            displayGrading.AddItem(new LabelItem(""));
+            Display displayInput = new Display(new Proportion() { Width = 30, TopLeft = new System.Drawing.Point(3, 3) });
+            displayInput.AddItem(new LabelItem("---- zadávání předmětu ----"));
+            displayInput.AddItem(new LabelItem("Předmět", ""));
+            Display displayInput2 = new Display(new Proportion() { Width = 30, TopLeft = new System.Drawing.Point(38, 3) });
+            displayInput2.AddItem(new LabelItem("---- zadávání známky ----"));
+            displayInput2.AddItem(new LabelItem("Známka", ""));
+            Display displayConfirm = new Display(new Proportion() { Width = 30, TopLeft = new System.Drawing.Point(25, 12) });
+            displayConfirm.AddItem(new LabelItem("Chceš vložit dalšího? [A]", ""));
+
+            Grade[] grades = new Grade[9];
+            grades[0] = new Grade() { Subject = "MAT", Score = 1 };
+            grades[1] = new Grade() { Subject = "CJL", Score = 4 };
+            grades[2] = new Grade() { Subject = "PRG", Score = 1 };
+            grades[3] = new Grade() { Subject = "MAT", Score = 2 };
+            grades[4] = new Grade() { Subject = "CJL", Score = 5 };
+            grades[5] = new Grade() { Subject = "CJL", Score = 3 };
+            grades[6] = new Grade() { Subject = "PRG", Score = 1 };
+            grades[7] = new Grade() { Subject = "MAT", Score = 2 };
+            grades[8] = new Grade() { Subject = "MAT", Score = 2 };
+
+            
+            ConsoleKeyInfo result;
+            do
+            {
+                int grade;
+                string temp;
+                displayInput.Refresh();
+                temp = Console.ReadLine();
+                displayInput2.Refresh();
+                int.TryParse(Console.ReadLine(), out grade);
+                if (temp.Length == 3 && grade < 6 && grade > 0) table.AddGrade(new Grade() { Score = grade, Subject = temp });
+
+                displayConfirm.Refresh();
+                result = Console.ReadKey();
+                Console.Clear();
+
+            } while (result.Key == ConsoleKey.A || result.Key == ConsoleKey.Enter);
+
+            foreach (var grade in grades)
+            {
+                GradeAvg ga = table.AddGrade(grade);
+            }
+
+            foreach (var item in table.GetAllGrades())
+            {
+                displayGrading.AddItem(new LabelItem(item.Subject, item.GetAverage()));
+            }
+            
+            Console.Clear();
+            displayGrading.Refresh();
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}

1c61a23cfd8f4549dad108d59551e65fe0789ab2
unknown <olibene018@ad.pslib.cz>
2019-11-08 08:18:43 +0100

    initial code

--- DisplayHelper/LabelItem.cs
+++ DisplayHelper/LabelItem.cs
@@ -22,7 +22,7 @@ namespace DisplayHelper
 
         public string Value
         {
-            get { return _value.ToString(); }
+            get { return _value?.ToString(); }
         }
 
         public override string ToString()
